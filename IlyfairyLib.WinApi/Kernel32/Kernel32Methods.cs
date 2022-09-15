c761d94b4d4e0282de4d19150567ab2d1ff44f66
小仙女 <me@ilyfairy.com>
2022-09-15 17:48:45 +0800

    update

---
+++ IlyfairyLib.WinApi/Kernel32/Kernel32Methods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace IlyfairyLib.WinApi.Kernel32
+{
+    public static class Kernel32Methods
+    {
+        [DllImport("kernel32.dll", EntryPoint = "GetQueuedCompletionStatus", ExactSpelling = true)]
+        static extern unsafe int GetQueuedCompletionStatus(nint CompletionPort, uint* lpNumberOfBytesTransferred, nuint* CompletionKey, nint* lpOverlapped, int dwMilliseconds);
+    }
+}
